12adf97d0fbb192f03ff79bc16cb8a22c76c91cd
Satria Adi <satria@Satrias-MacBook-Pro.local>
2026-04-14 11:10:33 +0700

    model

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -1,4 +1,8 @@
 @extends('base.base')
 @section('content')
     <h1>Store Page</h1>
+    @foreach ($product_categories as $pc)
+        <h2>{{ $pc->name }}</h2>
+        <p>{{ $pc->description }}</p>
+    @endforeach
 @endsection
